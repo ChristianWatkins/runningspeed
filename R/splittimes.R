9df23c6a94c27171af951900757a5242b172aa95
chwat467 <christian.watkins@tv2.no>
2023-01-19 14:29:33 +0100

    Added splittime function

---
+++ R/splittimes.R
@@ -0,0 +1,25 @@
+#' Calculate split times
+#'
+#' Add distance and time in seconds to get splitimes per 100 meter in seconds
+#'
+#' @param distanse Distance in meters.
+#' @param tid Time in seconds.
+#' Example splitimes(400, 60)
+
+splitimes <- function(distanse, tid) {
+  
+  # splittider
+  
+  # Antall splitter
+  splits <- distanse/100 # 400 meter gir 4. 100, 200, 300 og mål
+  
+  # Tid per split
+  splittime <- tid / splits
+  
+  # Loop gjennom
+  for (x in 1:splits) {
+      print(paste(x * 100, "meter:", round(x * splittime, 1), "sek"))
+  } 
+  
+  
+}
