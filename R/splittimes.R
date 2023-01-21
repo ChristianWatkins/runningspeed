90645f542ad0c2c4f00ad10857cb97231ce5f8a0
chwat467 <christian.watkins@tv2.no>
2023-01-21 11:29:23 +0100

    more advanced splittimes

--- R/splittimes.R
+++ R/splittimes.R
@@ -6,19 +6,36 @@
 #' @param tid Time in seconds.
 #' Example splitimes(400, 60)
 
-splitimes <- function(distanse, tid) {
+splittimes <- function(distanse, tid) {
   
-  # splittider
+# SHORT DISTANCES
   
-  # Antall splitter
-  splits <- distanse/100 # 400 meter gir 4. 100, 200, 300 og mål
+  # Get number of splits
+  if(distanse <= 200){
+  splits <-  distanse / 50  # 150 gir 3, 200 gir 4
   
-  # Tid per split
+  # Time per split
   splittime <- tid / splits
   
-  # Loop gjennom
-  for (x in 1:splits) {
+  # Print split times
+    for (x in 1:splits) {
+      print(paste(x * 50, "meter:", round(x * splittime, 1), "sek"))
+    } 
+  }
+
+# LONGER DISTANCES
+  
+  # Get number of splits
+  if(distanse > 200){
+    splits <-  distanse / 100  #  400 gir 4
+    
+    # Time per split
+    splittime <- tid / splits
+    
+    # Print split times 
+    for (x in 1:splits) {
       print(paste(x * 100, "meter:", round(x * splittime, 1), "sek"))
-  } 
+    } 
+  }  
   
 }
